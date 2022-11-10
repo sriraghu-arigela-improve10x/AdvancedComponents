610706b2c08aedabc0500a9f9a2f4371fa9a249f
Arigela sriraghu <sriraghu.arigela@improve10x.com>
2022-11-10 15:30:27 +0530

    implemented delete button functionality

--- app/src/main/java/com/example/advancedcomponents/TeamListActivity.java
+++ app/src/main/java/com/example/advancedcomponents/TeamListActivity.java
@@ -24,11 +24,21 @@ public class TeamListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_team_list);
         getSupportActionBar().setTitle("Team List");
         handleTeamListAddButton();
-
+        handleDeleteButton();
         setupTeamListData();
-        setupListViewTeamList();
+        setupListView();
     }
 
+    public void handleDeleteButton() {
+        Button deleteBtn = findViewById(R.id.delate_btn);
+        deleteBtn.setOnClickListener(view -> {
+            EditText memberNameTxt = findViewById(R.id.member_name_txt);
+            String memberName = memberNameTxt.getText().toString();
+            arrayAdapter.remove(memberName);
+            arrayAdapter.notifyDataSetChanged();
+            memberNameTxt.setText("");
+        });
+    }
 
     public void handleTeamListAddButton() {
         Button addBtn = findViewById(R.id.add_btn);
@@ -49,21 +59,15 @@ public class TeamListActivity extends AppCompatActivity {
         teamList.add("Renuka");
     }
 
-    public void setupListViewTeamList() {
+    public void setupListView() {
         teamListLv = findViewById(R.id.team_list_lv);
         arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, teamList);
         teamListLv.setAdapter(arrayAdapter);
-        teamListLv.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
-                Toast.makeText(TeamListActivity.this, "Team List : " + teamList.get(position), Toast.LENGTH_SHORT).show();
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
+        teamListLv.setOnItemClickListener((adapterView, view, position, l) -> {
+            String memberName = teamList.get(position);
+            EditText memberNameTxt = findViewById(R.id.member_name_txt);
+            memberNameTxt.setText(memberName);
+            Toast.makeText(this, "Click : " + teamList.get(position), Toast.LENGTH_SHORT).show();
         });
     }
 }
